9cac2f1ccb362aa1a8d45dec97e7ab2c9ebdd53f
irturhamun <irmansyah.turhamun@ui.ac.id>
2019-03-05 22:45:19 +0700

    array

---
+++ array-concat.js
@@ -0,0 +1,8 @@
+function panggilConcat() {
+  var kota1 = ['jakarta','bogor','depok','bekasi']
+  var kota2 = ['bandung','cimahi','tasikmalaya','brebes']
+  var hasil = kota1.concat(kota2) //menggabungkan dua buah array
+  return hasil;
+}
+
+console.log(panggilConcat());
--- array-indexof.js
+++ array-indexof.js
@@ -2,5 +2,6 @@ function panggilIndexOf() {
   var kota = ['jakarta','bekasi','depok','bogor','tanggerang'];
 
   return kota.indexOf("bogor"); //pencarian index dari kata di dalam array
+  // hasil rumus (sesuai posisi jika ada dan -1 jika tidak ada)
 }
 console.log(panggilIndexOf())
