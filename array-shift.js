66f73a5637e4f3626e90ef7b4ceaa5efc7ebf5b1
irturhamun <irmansyah.turhamun@ui.ac.id>
2019-03-05 15:06:39 +0700

    shifting array

---
+++ array-shift.js
@@ -0,0 +1,11 @@
+function panggilShiftA() {
+  var kota = ['jakarta','depok','bekasi','tanggerang','seribu'];
+  console.log(kota);
+  console.log("==========");
+//  kota = kota.shift(); //isi kota menjadi nilai yang di shift kiri pada array
+var kota2 = kota.shift()
+console.log(kota2)
+  return kota; //variable kota akan dihapus menggunakan shift
+}
+console.log(panggilShiftA()); //return tampil karena console.log
+//panggilShiftA(); //tidak ada tampilan return karena tidak menggunakan console.log
