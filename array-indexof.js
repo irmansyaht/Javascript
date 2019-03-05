1ac1bb8c16e048908555460afb6724119f7064d6
irturhamun <irmansyah.turhamun@ui.ac.id>
2019-03-05 22:39:07 +0700

    array

---
+++ array-indexof.js
@@ -0,0 +1,6 @@
+function panggilIndexOf() {
+  var kota = ['jakarta','bekasi','depok','bogor','tanggerang'];
+
+  return kota.indexOf("bogor"); //pencarian index dari kata di dalam array
+}
+console.log(panggilIndexOf())
---
+++ array-unshift.js
@@ -0,0 +1,7 @@
+function panggilUnshiftA() {
+  var data = ["jakarta","depok","bekasi","tanggerang"]
+console.log(data);
+  data.unshift( "cileungsi" , "seribu" );
+return data;
+}
+console.log(panggilUnshiftA());
